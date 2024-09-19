6668132cae3f9caf1625e15697a9192c8956c35a
Elyzence <158237077+Elyzence@users.noreply.github.com>
2024-09-19 11:06:23 +0800

    Delete js directory

--- js/aa4a6fa7a80535e1.en-GB.js
+++
@@ -1,9 +0,0 @@
-window["cmsg"]["assets"] = window["cmsg"]["assets"] || {};
-window["cmsg"]["assets"]["en-GB"] = Object.assign(
-  window["cmsg"]["assets"]["en-GB"] || {},
-  {
-    3499: { js: ["34db8819180b899b.strings.js"], css: [] },
-    4158: { js: ["c1e0961c2e4da606.strings.js"], css: [] },
-    8528: { js: ["1574648abab766c8.strings.js"], css: [] },
-  }
-);
